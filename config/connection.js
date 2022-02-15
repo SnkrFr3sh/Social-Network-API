9068efd22dd5f64db03742ab1fc3681148c6be28
Marc Pacampara <mpacampara@gmail.com>
2022-02-14 20:35:18 -0800

    updated connection.js to allow others to connect to mongodb

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 const {connect, connection}  = require('mongoose');
 
-connect('mongodb://127.0.0.1:27017/socialnetworkapi', 
-{
+const connectionString = 
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkapi';
+
+connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
